3f79d4566cc4427feaee64e9bb0d9153cb3e8965
ManonLef <manonlefkos@gmail.com>
2022-03-27 22:13:35 +0200

    add computerPlay function

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-let playerChoice = prompt("weapon?").toLowerCase();
+let playerSelection = prompt("weapon?").toLowerCase();
+let computerSelection;
 
+function computerPlay() {
+    let random = Math.floor(Math.random()*3)+1;
+    
+    if (random === 1) {
+        return "rock";
+    } else if (random === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+
+
+console.log(computerPlay(computerSelection));
+console.log(playerSelection);
