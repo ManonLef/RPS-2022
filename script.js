e3366d6411f28d1d8e879183bef90f804259a2f0
Manon <81025586+ManonLef@users.noreply.github.com>
2022-06-23 21:54:36 +0200

    comment out 5 rounds

--- script.js
+++ script.js
@@ -23,30 +23,30 @@ function playRound(computerChoice, playerChoice) {
 } 
 
 
-function game() {
-    for (let i = 0; i <= 5; i++) {
-        if (i < 5) {
-            let playerSelection = prompt("What will be your weapon of choice?\nRock, Paper or Scissors?").toLowerCase();
-            let computerSelection = computerPlay();
+// function game() {
+//     for (let i = 0; i <= 5; i++) {
+//         if (i < 5) {
+//             let playerSelection = prompt("What will be your weapon of choice?\nRock, Paper or Scissors?").toLowerCase();
+//             let computerSelection = computerPlay();
 
-            if (playerSelection === "rock" 
-                || playerSelection === "paper" 
-                || playerSelection === "scissors") {
-                console.log(playRound(computerSelection, playerSelection));
-            } else {
-                i--;
-                console.log( "Pick again, but this time only 'rock', 'paper' or 'scissors'");
-                continue;
-            }
-        } else {
-            if (computerScore > playerScore) {
-                console.log("You lost, Computer score is " + computerScore + " You scored " + playerScore)
-            } else {
-                console.log("You win, Computer score is " + computerScore + " You scored " + playerScore)
-            }
-        }
-    }
-}
+//             if (playerSelection === "rock" 
+//                 || playerSelection === "paper" 
+//                 || playerSelection === "scissors") {
+//                 console.log(playRound(computerSelection, playerSelection));
+//             } else {
+//                 i--;
+//                 console.log( "Pick again, but this time only 'rock', 'paper' or 'scissors'");
+//                 continue;
+//             }
+//         } else {
+//             if (computerScore > playerScore) {
+//                 console.log("You lost, Computer score is " + computerScore + " You scored " + playerScore)
+//             } else {
+//                 console.log("You win, Computer score is " + computerScore + " You scored " + playerScore)
+//             }
+//         }
+//     }
+// }
 
-game();
+// game();
 
